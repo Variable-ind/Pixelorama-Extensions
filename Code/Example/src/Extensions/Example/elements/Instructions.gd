466168717ce0845c531e4a2692d6384607b02c49
Variable <77773850+Variable-ind@users.noreply.github.com>
2023-04-25 20:18:12 +0500

    Example v0.6

--- Code/Example/src/Extensions/Example/elements/Instructions.gd
+++ Code/Example/src/Extensions/Example/elements/Instructions.gd
@@ -1,16 +1,7 @@
 extends WindowDialog
 
+var _global :Node  # Needed for reference to "Global" node of Pixelorama (Used most of the time)
 
-####################################################
-###                                              ###
-########       Enter Your code Here         ########
-
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_global = get_node_or_null("/root/Global")
 	set_global_position(OS.window_size/2.0 - rect_size/2.0)
@@ -23,14 +14,11 @@ func _on_Button_pressed() -> void:
 
 func _on_Create_pressed() -> void:
 	$NewExtension.popup_centered()
-
-###                                             ####
-####################################################
+	_global.dialog_open(true)
 
 
-
-#### You don't have to touch anything below
-var _global :Node  # Needed for reference to "Global" node of Pixelorama (Used most of the time)
+func _on_NewExtension_popup_hide() -> void:
+	_global.dialog_open(false)
 
 
 func _input(event: InputEvent) -> void:
@@ -44,5 +32,3 @@ func _input(event: InputEvent) -> void:
 			else:
 				if _global.has_focus:
 					_global.can_draw = true
-
-
