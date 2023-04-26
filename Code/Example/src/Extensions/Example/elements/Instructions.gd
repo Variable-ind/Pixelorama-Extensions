64c7d9f50e1027d98ed8ab76b2ad9db31a513578
Variable <77773850+Variable-ind@users.noreply.github.com>
2023-04-26 13:07:09 +0500

    new version of example extension
    UI improvements

--- Code/Example/src/Extensions/Example/elements/Instructions.gd
+++ Code/Example/src/Extensions/Example/elements/Instructions.gd
@@ -1,34 +1,13 @@
 extends WindowDialog
 
-var _global :Node  # Needed for reference to "Global" node of Pixelorama (Used most of the time)
 
-func _ready() -> void:
-	_global = get_node_or_null("/root/Global")
-	set_global_position(OS.window_size/2.0 - rect_size/2.0)
-	show()
-
-
-func _on_Button_pressed() -> void:
-	OS.clipboard = $VBoxContainer/Content/VBoxContainer/HBoxContainer/Code.text
+func _on_Creator_pressed() -> void:
+	self.popup_centered()
 
 
 func _on_Create_pressed() -> void:
 	$NewExtension.popup_centered()
-	_global.dialog_open(true)
 
 
-func _on_NewExtension_popup_hide() -> void:
-	_global.dialog_open(false)
-
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouse:
-		var mouse_pos = get_local_mouse_position()
-
-		# Avoid canvas interaction if hovering above dialog
-		if _global:
-			if Rect2(Vector2.ZERO, rect_size).has_point(mouse_pos):
-				_global.can_draw = false
-			else:
-				if _global.has_focus:
-					_global.can_draw = true
+func _on_Copy_pressed() -> void:
+	OS.clipboard = $PanelContainer/Content/VBoxContainer/HBoxContainer/Code.text
--- Code/Example/src/Extensions/Example/elements/NewExtension.gd
+++ Code/Example/src/Extensions/Example/elements/NewExtension.gd
@@ -16,33 +16,33 @@ var extension_json := {
 
 
 func _on_NewExtension_about_to_show() -> void:
-	$VBoxContainer/Save/path.text = ProjectSettings.globalize_path("res://")
+	$PanelContainer/VBoxContainer/Save/path.text = ProjectSettings.globalize_path("res://")
 
 
 func _on_Name_text_changed(new_text: String) -> void:
 	if new_text == "":
-		extension_json.name = $VBoxContainer/Name/LineEdit.placeholder_text
+		extension_json.name = $PanelContainer/VBoxContainer/Name/LineEdit.placeholder_text
 	else:
 		extension_json.name = new_text
 
 
 func _on_DisplayName_text_changed(new_text: String) -> void:
 	if new_text == "":
-		extension_json.display_name = $VBoxContainer/DisplayName/LineEdit.placeholder_text
+		extension_json.display_name = $PanelContainer/VBoxContainer/DisplayName/LineEdit.placeholder_text
 	else:
 		extension_json.display_name = new_text
 
 
 func _on_Description_text_changed(new_text: String) -> void:
 	if new_text == "":
-		extension_json.description = $VBoxContainer/Description/LineEdit.placeholder_text
+		extension_json.description = $PanelContainer/VBoxContainer/Description/LineEdit.placeholder_text
 	else:
 		extension_json.description = new_text
 
 
 func _on_Author_text_changed(new_text: String) -> void:
 	if new_text == "":
-		extension_json.author = $VBoxContainer/Author/LineEdit.placeholder_text
+		extension_json.author = $PanelContainer/VBoxContainer/Author/LineEdit.placeholder_text
 	else:
 		extension_json.author = new_text
 
@@ -63,16 +63,16 @@ func _on_License_text_changed(new_text: String) -> void:
 
 
 func _on_Choose_pressed() -> void:
-	$FileDialog.popup()
-	$FileDialog.current_dir = $VBoxContainer/Save/path.text
+	$FileDialog.popup_centered()
+	$FileDialog.current_dir = $PanelContainer/VBoxContainer/Save/path.text
 
 
 func _on_FileDialog_dir_selected(dir: String) -> void:
-	$VBoxContainer/Save/path.text = dir
+	$PanelContainer/VBoxContainer/Save/path.text = dir
 
 
 func _on_NewExtension_confirmed() -> void:
-	var save_path = $VBoxContainer/Save/path.text
+	var save_path = $PanelContainer/VBoxContainer/Save/path.text
 	if save_path.ends_with("/"):
 		save_path[-1] = ""
 	save_path += str("/", extension_json.name, "/")
@@ -94,7 +94,7 @@ func _on_NewExtension_confirmed() -> void:
 	file.store_string(var2str(extension_json))
 	file.close()
 
-	#Step 3 : make relevant files
+	# Step 3 : make relevant files
 	var main_path = extension_path.plus_file("Main.tscn")
 	var main_script_path = extension_path.plus_file("Main.gd")
 	var extension_api_path = extension_path.plus_file("ExtensionsApi.gd")
@@ -108,7 +108,7 @@ func _on_NewExtension_confirmed() -> void:
 	file.close()
 # warning-ignore:return_value_discarded
 	file.open(extension_api_path, File.WRITE)
-	file.store_string($Hidden/ApiVersions.get_child($VBoxContainer/ApiLevel/Api.selected).text)
+	file.store_string($Hidden/ApiVersions.get_child($PanelContainer/VBoxContainer/ApiLevel/Api.selected).text)
 	file.close()
 # warning-ignore:return_value_discarded
 	file.open(save_path.plus_file("project.godot"), File.WRITE)
