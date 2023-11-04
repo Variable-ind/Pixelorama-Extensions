5b441b9602bd9547fd9126472cde1925a07725e8
Variable <77773850+Variable-ind@users.noreply.github.com>
2023-11-04 10:41:56 +0500

    Added the ColorPickerDocker extension

---
+++ Code/ColorPickerDocker/src/Extensions/ColorPickerDocker/API/EmptyClasses/AnimationTag.gd
@@ -0,0 +1,7 @@
+class_name AnimationTag
+extends Reference
+# A class for frame tag properties
+
+
+func _init(_name, _color, _from, _to) -> void:
+	pass
---
+++ Code/ColorPickerDocker/src/Extensions/ColorPickerDocker/API/EmptyClasses/Tiles.gd
@@ -0,0 +1,6 @@
+class_name Tiles
+extends Reference
+
+
+func _init(size: Vector2):
+	pass
