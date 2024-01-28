e3192f24d4c61915d764a39ad32bf72c799c062e
Variable <pkfayezakhtar@gmail.com>
2024-01-28 20:52:48 +0500

    update keydisplay

--- Code/KeyDisplay/src/Extensions/KeyDisplay/KeyDisplay.gd
+++ Code/KeyDisplay/src/Extensions/KeyDisplay/KeyDisplay.gd
@@ -2,59 +2,66 @@ extends GridContainer
 
 var main_canvas: Control
 
-var key_array := PoolStringArray()
+var key_array := PackedStringArray()
 
-onready var history_container: VBoxContainer = $"%HistoryContainer"
-onready var key_container: HBoxContainer = $"%KeyContainer"
+@onready var history_container: VBoxContainer = %HistoryContainer
+@onready var key_container: HBoxContainer = %KeyContainer
 
+var last_key_release_time: int
 
 func _ready() -> void:
-	main_canvas = ExtensionsApi.general.get_global().control.find_node("Main Canvas")
-# warning-ignore:return_value_discarded
-	main_canvas.connect("item_rect_changed", self, "update_position")
-# warning-ignore:return_value_discarded
-	connect("item_rect_changed", self, "update_position")
+	var extensions_api = get_node_or_null("/root/ExtensionsApi")
+	main_canvas = extensions_api.general.get_global().control.find_child("Main Canvas")
+	main_canvas.item_rect_changed.connect(update_position)
+	item_rect_changed.connect(update_position)
 	update_position()
 
 
 func update_position():
 	if main_canvas:
-		rect_global_position = main_canvas.rect_global_position + main_canvas.rect_size - rect_size
+		global_position = main_canvas.global_position + main_canvas.size - size
 
 
 func update_text():
 	move_to_history()
 	for text in key_array:
-		var key = preload("res://src/Extensions/KeyDisplay/Elements/Key.tscn").instance()
+		var key = preload("res://src/Extensions/KeyDisplay/Elements/Key.tscn").instantiate()
 		key.text = text
 		key_container.add_child(key)
 
 
 func move_to_history():
 	var old_keys = HBoxContainer.new()
+	# determine a suitable fade time
 	var fade_time := 0.5
-	if key_array.size() > key_container.get_child_count():
+	if key_array.size() > key_container.get_child_count():  # A new key was pressed
 		fade_time = 0
-	old_keys.alignment = BoxContainer.ALIGN_END
-	old_keys.modulate = Color.gray
-	history_container.add_child(old_keys)
+	else:  # A key was released
+		# if some keys were released very close together then ignore them
+		if Time.get_ticks_msec() - last_key_release_time < 50:
+			fade_time = 0
+		last_key_release_time = Time.get_ticks_msec()
+
 	for key in key_container.get_children():
+		if key.text not in key_array:
+			key.modulate = Color.YELLOW
 		key_container.remove_child(key)
 		old_keys.add_child(key)
+	old_keys.alignment = BoxContainer.ALIGNMENT_END
+	old_keys.modulate = Color.GRAY
+	history_container.add_child(old_keys)
 
 	var tween := get_tree().create_tween()
-# warning-ignore:return_value_discarded
-	tween.tween_property(old_keys, "modulate", Color.transparent, fade_time)
-# warning-ignore:return_value_discarded
-	tween.tween_callback(old_keys, "queue_free")
+	tween.tween_property(old_keys, "modulate", Color.TRANSPARENT, fade_time)
+	tween.tween_callback(old_keys.queue_free)
 
 
 func _input(event: InputEvent) -> void:
 	var mouse_buttons = $Mouse
 	if event is InputEventKey:
-		var key = OS.get_scancode_string(event.physical_scancode)
+		var key = OS.get_keycode_string(event.physical_keycode)
 		if event.pressed:
-			if !key_array.empty():
+			if !key_array.is_empty():
 				if !key_array.has(key):
 					key_array.append(key)
 				else:
@@ -62,16 +69,16 @@ func _input(event: InputEvent) -> void:
 			else:
 				key_array = [key]
 		else:
-			key_array.remove(key_array.find(key))
+			key_array.remove_at(key_array.find(key))
 		update_text()
 
 	if event is InputEventMouseButton:
 		if event.pressed:
-			if event.button_index == BUTTON_LEFT:
+			if event.button_index == MOUSE_BUTTON_LEFT:
 				mouse_buttons.get_node("Left").visible = true
-			elif event.button_index == BUTTON_RIGHT:
+			elif event.button_index == MOUSE_BUTTON_RIGHT:
 				mouse_buttons.get_node("Right").visible = true
-			elif event.button_index == BUTTON_MIDDLE:
+			elif event.button_index == MOUSE_BUTTON_MIDDLE:
 				mouse_buttons.get_node("Middle").visible = true
 		else:
 			mouse_buttons.get_node("Left").visible = false
@@ -80,5 +87,5 @@ func _input(event: InputEvent) -> void:
 
 
 func _exit_tree() -> void:
-	main_canvas.disconnect("item_rect_changed", self, "update_position")
+	main_canvas.item_rect_changed.disconnect(update_position)
 	queue_free()
--- store_info.md
+++ store_info.md
@@ -1,6 +1,8 @@
 // This file is for online use.<br>
 // My extensions for Pixelorama 1.x
 
+[["name", "KeyDisplay"], ["version", 1.0], ["description", "Handy extension to display shortcuts for use in videos etc.)"], ["tags", "UI"], ["thumbnail", "https://github.com/Variable-ind/Pixelorama-Extensions/assets/77773850/dcedd193-1c37-4311-8dc2-ee0b65de8cec"], ["download_link", "https://github.com/Variable-ind/Pixelorama-Extensions/raw/4.0/Extensions/KeyDisplay.pck"]]
+
 [["name", "Swappy"], ["version", 0.2], ["description", "Helper Extension for Re-Coloring. Replaces all instances of a Color with a New Color."], ["tags", "Tool", "UI"], ["thumbnail", "https://user-images.githubusercontent.com/77773850/246190032-e4d1e2b9-03d8-4a6d-9834-e29ca7fbf463.png"], ["download_link", "https://github.com/Variable-ind/Pixelorama-Extensions/raw/4.0/Extensions/Swappy.pck"]]
 
 [["name", "TimeTracking"], ["version", 0.2], ["description", "Saves project statistics in the .pxo file (access with Help>Project Statistics)"], ["tags", "Analytics"], ["thumbnail", "https://user-images.githubusercontent.com/77773850/272894172-5c2aec0e-944e-4e91-90ee-8fe1803cf83d.png"], ["download_link", "https://github.com/Variable-ind/Pixelorama-Extensions/raw/4.0/Extensions/TimeTracking.pck"]]
